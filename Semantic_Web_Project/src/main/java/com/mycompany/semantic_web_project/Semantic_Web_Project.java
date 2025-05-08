3c92fa203f313d7924dd2d329b1df211a4571e79
Mahmoud Alsoudy <113215972+Mahmoudshar0@users.noreply.github.com>
2025-05-08 08:48:20 +0300

    update

--- Semantic_Web_Project/src/main/java/com/mycompany/semantic_web_project/Semantic_Web_Project.java
+++ Semantic_Web_Project/src/main/java/com/mycompany/semantic_web_project/Semantic_Web_Project.java
@@ -27,7 +27,7 @@ public class Semantic_Web_Project
         
         System.out.println("Hello World!");
     
-        // Define the IRI for the class ProgrammingBook
+        // List all individuals in class ProgrammingBook
         OWLClass programmingBookClass = factory.getOWLClass(
         IRI.create("http://www.semanticweb.org/mo7am/ontologies/2025/3/library#ProgrammingBook")
         );
@@ -40,6 +40,37 @@ public class Semantic_Web_Project
         for (OWLNamedIndividual individual : individuals.getFlattened()) {
         System.out.println(" - " + individual.getIRI().getShortForm());
         }
+        
+        //  ------------------------------------------------------------------------------------
 
+        // Step 1: Define the required IRIs
+        IRI hasAuthorIRI = IRI.create("http://www.semanticweb.org/mo7am/ontologies/2025/3/library#hasAuthor");
+        IRI recommendedForIRI = IRI.create("http://www.semanticweb.org/mo7am/ontologies/2025/3/library#recommendedFor");
+        IRI authorIRI = IRI.create("http://www.semanticweb.org/mo7am/ontologies/2025/3/library#Author");
+        IRI difficultyIRI = IRI.create("http://www.semanticweb.org/mo7am/ontologies/2025/3/library#DifficultyLevel");
+
+        // Step 2: Create object properties
+        OWLObjectProperty hasAuthor = factory.getOWLObjectProperty(hasAuthorIRI);
+        OWLObjectProperty recommendedFor = factory.getOWLObjectProperty(recommendedForIRI);
+
+        // Step 3: Create class expressions for the DL query
+        OWLClass authorClass = factory.getOWLClass(authorIRI);
+        OWLClass difficultyClass = factory.getOWLClass(difficultyIRI);
+
+        OWLClassExpression hasAuthorSomeAuthor = factory.getOWLObjectSomeValuesFrom(hasAuthor, authorClass);
+        OWLClassExpression recommendedForSomeDifficulty = factory.getOWLObjectSomeValuesFrom(recommendedFor, difficultyClass);
+
+        // Step 4: Combine both with AND (intersection)
+        OWLClassExpression combinedQuery = factory.getOWLObjectIntersectionOf(hasAuthorSomeAuthor, recommendedForSomeDifficulty);
+
+        // Step 5: Use reasoner to find all individuals matching the DL query
+        NodeSet<OWLNamedIndividual> results = reasoner.getInstances(combinedQuery, false);
+
+        // Step 6: Print results
+        System.out.println("\n🔍 Individuals matching: hasAuthor some Author AND recommendedFor some DifficultyLevel");
+        for (OWLNamedIndividual ind : results.getFlattened()) {
+            System.out.println(" - " + ind.getIRI().getShortForm());
+        }
+        
     }
 }
